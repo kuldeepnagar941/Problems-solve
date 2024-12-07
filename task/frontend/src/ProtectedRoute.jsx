2b17cedd08b6dea9d84f206c6d5d1e3080dc3b99
kuldeepnagar941 <kuldeepnagar941@gmail.com>
2024-12-07 17:02:41 +0530

    completed

--- task/frontend/src/ProtectedRoute.jsx
+++ task/frontend/src/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({pro}) => {
   const token = localStorage.getItem("token");
 
-  // If token exists, render the protected element; otherwise, redirect to login
+  
   return token ? pro : <Navigate to="/login" replace />;
 };
 
